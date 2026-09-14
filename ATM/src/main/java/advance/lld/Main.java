437e7f014baeb71d5ddbde9b5561017c015b5414
vishal kumar <vkumar220198@gmail.com>
2026-09-14 18:21:02 +0530

    added main file

---
+++ ATM/src/main/java/advance/lld/Main.java
@@ -0,0 +1,12 @@
+package advance.lld;
+
+import advance.lld.ClientDemo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ClientDemo clientDemo = new ClientDemo();
+        clientDemo.demo();
+    }
+}
